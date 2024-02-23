f7caf5ab38948931665230e952e99a97d538cfdf
clair234 <clairhany2017@gmail.com>
2024-02-23 05:28:03 -0800

    update

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
---script that updates the score
-update second_table
-set score = 10
-where name = "Bob";
+-- Updates the score of Bob to 10 in the table second_table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
